f8b1a0009ed033a78baab8e0e2b0a5ffe1910ea2
jalopino <50796215+jalopino@users.noreply.github.com>
2022-10-25 23:49:27 +0800

    Add files via upload

---
+++ src/main/java/com/example/layout/main.java
@@ -0,0 +1,13 @@
+package com.example.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class main extends Activity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
